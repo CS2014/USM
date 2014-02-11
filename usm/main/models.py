56c1a371045af7114dd5ff8f4f957a957e04fb68
Ross Kinsella <rosskinsella0@gmail.com>
2014-02-11 16:02:41 +0000

    Added commenting

--- usm/main/models.py
+++ usm/main/models.py
@@ -1,8 +1,18 @@
+'''
+	author: Ross Kinsella
+	date:   2014/feb/11
+'''
+
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
 class Society(models.Model):
+	'''
+	A master class which Accounts, Communication classes,
+	Meetings, Minutes and Society members belong to.
+	'''
 	name = models.CharField(max_length=50)
 	members = models.ManyToManyField(User)
 	creation_date = models.DateTimeField(default=timezone.now, editable=False)
