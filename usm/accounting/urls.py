7996a1bd9a5c9a1774d7f2a21026acb2aac115ec
Ross Kinsella <rosskinsella0@gmail.com>
2014-02-17 19:32:33 +0000

    All accounting models now have views for more detail and edit

--- usm/accounting/urls.py
+++ usm/accounting/urls.py
@@ -5,7 +5,7 @@ from accounting import views
 urlpatterns = patterns('accounting.views',
 
 	#indexs
-    url(r'^$', views.index),
+    url(r'^$', 'index'),
     url(r'^accounts/$', views.account_index),
    	url(r'^transaction_categories/$', views.transaction_category_index),
     url(r'^transaction_methods/$', views.transaction_method_index),
@@ -13,7 +13,6 @@ urlpatterns = patterns('accounting.views',
     url(r'^bills/$', views.bill_index),
     url(r'^invoices/$', views.invoice_index),
 	url(r'^grants/$', views.grant_index),
-
 
     #Creates
     url(r'^accounts/new', views.account_new),
@@ -24,4 +23,21 @@ urlpatterns = patterns('accounting.views',
     url(r'^invoices/new', views.invoice_new),
 	url(r'^grants/new', views.grant_new),
 
+    #Details
+    url(r'accounts/(?P<id>\d+)/$', views.account_detail),   
+    url(r'transaction_categories/(?P<id>\d+)/$', views.transaction_category_detail),
+    url(r'transaction_methods/(?P<id>\d+)/$', views.transaction_method_detail),
+    url(r'transactions/(?P<id>\d+)/$', views.transaction_detail),
+    url(r'bills/(?P<id>\d+)/$', views.bill_detail),
+    url(r'invoices/(?P<id>\d+)/$', views.invoice_detail),
+    url(r'grants/(?P<id>\d+)/$', views.grant_detail),
+
+    #Edits
+    url(r'accounts/(?P<id>\d+)/edit$', views.account_edit),
+    url(r'transaction_categories/(?P<id>\d+)/edit$', views.transaction_category_edit),
+    url(r'transaction_methods/(?P<id>\d+)/edit$', views.transaction_method_edit),
+    url(r'transactions/(?P<id>\d+)/edit$', views.transaction_edit),
+    url(r'bills/(?P<id>\d+)/edit$', views.bill_edit),
+    url(r'invoices/(?P<id>\d+)/edit$', views.invoice_edit),
+    url(r'grants/(?P<id>\d+)/edit$', views.grant_edit),
 )
