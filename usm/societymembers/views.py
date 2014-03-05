6d47d745f7b9fa4e05a14a6e2227179b861abeda
Kevin <koflana@tcd.ie>
2014-03-05 13:44:15 +0000

    Added Stub for societymembers views

--- usm/societymembers/views.py
+++ usm/societymembers/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+'''
+		author: Kevin O'Flanagan
+		date:   2014/march/5
 
-# Create your views here.
+
+		TODO:
+		-Implement views
+
+'''
+
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponseRedirect
+from utils import build_pretty_data_view
+
+from societymembers.models import Tag, SocietyMember, MembershipFee
+from django.db.models import get_app, get_models
+from django.forms.models import model_to_dict
+from societymembers.models import TagForm, SocietyMemberForm, MembershipFeeForm
